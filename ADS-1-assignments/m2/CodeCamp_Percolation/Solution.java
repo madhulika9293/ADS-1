72f22d1f79d2d1a9669ee8ab47827e796887561c
Madhulika <ramyamdhulika9293@msitprogram.net>
2018-09-25 15:18:19 +0530

    "check_style_score -> 0 of 24 passed. style score: 0/1 "

---
+++ ADS-1-assignments/m2/CodeCamp_Percolation/Solution.java
@@ -0,0 +1,110 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+
+
+class Percolation {
+	// create n-by-n grid, with all sites blocked
+
+	/**
+	 * grid for simulation.
+	 */
+	private boolean[][] grid;
+	/**
+	 * Size of the grid.
+	 */
+	private int n;
+	/**
+	 * number of open sites.
+	 */
+	private int count;
+	/**
+	 * top node.
+	 */
+	private int top;
+	/**
+	 * bottom node.
+	 */
+	private int bottom;
+	/**
+	 * connection array.
+	 */
+	private WeightedUnion percCheck;
+
+	/**
+	 * Constructs the object.
+	 *
+	 * @param      num     Size of the grid.
+	 */
+	public Percolation(int num) {
+		grid = new boolean[num][num];
+		count = 0;
+		top = 0;
+		bottom = (num * num) + 1;
+		percCheck = new WeightedUnion(n * n + 2);
+		n = num;
+	}
+
+	public int getNum(final int row, final int col) {
+		return ((row - 1) * n + col) ;
+	}
+	// open site (row, col) if it is not open already
+	public void open(int row, int col) {
+		if (isOpen(row, col)) {
+			return;
+		}
+		grid[row - 1][col - 1] = true;
+		count += 1;
+		if (row == 1) {
+			percCheck.union(getNum(row, col), top);
+		}
+		if (row == n) {
+			percCheck.union(getNum(row, col), bottom);
+		}
+		if (isOpen(row, col - 1)) {
+			percCheck.union(getNum(row, col), getNum(row, col - 1));
+		}
+		if (isOpen(row, col + 1)) {
+			percCheck.union(getNum(row, col), getNum(row, col + 1));
+		}
+		if (isOpen(row - 1, col)) {
+			percCheck.union(getNum(row, col), getNum(row - 1, col));
+		}
+		if (isOpen(row + 1, col)) {
+			percCheck.union(getNum(row, col), getNum(row + 1, col));
+		}
+	}
+
+	public boolean isOpen(int row, int col) {
+		// is site (row, col) open?
+		return grid[row - 1][col - 1];
+	}
+	// public boolean isFull(int row, int col)  // is site (row, col) full?
+	public     int numberOfOpenSites() {
+		// number of open sites
+		return count;
+	}
+	public boolean percolates() {
+		// does the system percolate?
+		return percCheck.connected(top, bottom);
+	}
+}
+
+
+// You can implement the above API to solve the problem
+
+public class Solution {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int num = scan.nextInt();
+		Percolation perc = new Percolation(num);
+		// System.out.println(Arrays.deepToString(perc));
+		while (scan.hasNext()) {
+			int p = scan.nextInt();
+			int q = scan.nextInt();
+			perc.open(p, q);
+		}
+		System.out.println(perc.percolates());
+	}
+}
+
---
+++ ADS-1-assignments/m2/CodeCamp_Percolation/WeightedUnion.java
@@ -0,0 +1,59 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+class WeightedUnion {
+	/**
+	 * Array for the connections.
+	 */
+	private int[] id;
+	/**
+	 * Number of objects rooted in the array at i.
+	 */
+	private int[] sz;
+	/**
+	 * Constructs the object.
+	 *
+	 * @param      n     Total number of elements in the tree.
+	 */
+	WeightedUnion(int n) {
+		id = new int[n];
+		sz = new int[n];
+	}
+
+	/**
+	 * Searches for the first match.
+	 *
+	 * @param      p     Element in the array.
+	 *
+	 * @return     root of p.
+	 */
+	public int find(final int p) {
+		while (p != id[p]) {
+			id[p] = id[id[p]];
+		}
+		int temp = id[p];
+		return temp;
+	}
+
+	/**
+	 * checks if connected.
+	 *
+	 * @param      p     { parameter_description }
+	 * @param      q     The quarter
+	 *
+	 * @return     { description_of_the_return_value }
+	 */
+	public boolean connected(final int p, final int q) {
+		return find(p) == find(q);
+	}
+
+	public void union(final int p, final int q) {
+		int i = find(p);
+		int j = find(q);
+		if (i == j) return;
+
+		// make smaller root point to larger one
+		if   (sz[i] < sz[j]) { id[i] = j; sz[j] += sz[i]; }
+		else                 { id[j] = i; sz[i] += sz[j]; }
+	}
+}
