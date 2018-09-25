87787e2c682db6e857261a2d5892caf3d07fbab0
Madhulika <ramyamdhulika9293@msitprogram.net>
2018-09-25 16:23:37 +0530

    "check_style_score -> 24 of 24 passed. style score: 0/1 "

--- ADS-1-assignments/m2/CodeCamp_Percolation/WeightedUnion.java
+++ ADS-1-assignments/m2/CodeCamp_Percolation/WeightedUnion.java
@@ -2,62 +2,62 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 class WeightedUnion {
-	/**
-	 * Array for the connections.
-	 */
-	private int[] id;
-	/**
-	 * Number of objects rooted in the array at i.
-	 */
-	private int[] sz;
-	/**
-	 * Constructs the object.
-	 *
-	 * @param      n     Total number of elements in the tree.
-	 */
-	WeightedUnion(int number) {
-		id = new int[number];
-		sz = new int[number];
-		for (int i = 0; i < number; i++) {
-			id[i] = i;
-			sz[i] = 1;
-		}
-	}
+  /**
+   * Array for the connections.
+   */
+  private int[] id;
+  /**
+   * Number of objects rooted in the array at i.
+   */
+  private int[] sz;
+  /**
+   * Constructs the object.
+   *
+   * @param      n     Total number of elements in the tree.
+   */
+  WeightedUnion(int number) {
+    id = new int[number];
+    sz = new int[number];
+    for (int i = 0; i < number; i++) {
+      id[i] = i;
+      sz[i] = 1;
+    }
+  }
 
-	/**
-	 * Searches for the first match.
-	 *
-	 * @param      p     Element in the array.
-	 *
-	 * @return     root of p.
-	 */
-	public int find(int p) {
-		while (p != id[p]) {
-			id[p] = id[id[p]];
-			p = id[p];
-		}
-		return p;
-	}
+  /**
+   * Searches for the first match.
+   *
+   * @param      p     Element in the array.
+   *
+   * @return     root of p.
+   */
+  public int find(int p) {
+    while (p != id[p]) {
+      id[p] = id[id[p]];
+      p = id[p];
+    }
+    return p;
+  }
 
-	/**
-	 * checks if connected.
-	 *
-	 * @param      p     { parameter_description }
-	 * @param      q     The quarter
-	 *
-	 * @return     { description_of_the_return_value }
-	 */
-	public boolean connected(final int p, final int q) {
-		return find(p) == find(q);
-	}
+  /**
+   * checks if connected.
+   *
+   * @param      p     { parameter_description }
+   * @param      q     The quarter
+   *
+   * @return     { description_of_the_return_value }
+   */
+  public boolean connected(final int p, final int q) {
+    return find(p) == find(q);
+  }
 
-	public void union(final int p, final int q) {
-		int i = find(p);
-		int j = find(q);
-		if (i == j) return;
+  public void union(final int p, final int q) {
+    int i = find(p);
+    int j = find(q);
+    if (i == j) return;
 
-		// make smaller root point to larger one
-		if   (sz[i] < sz[j]) { id[i] = j; sz[j] += sz[i]; }
-		else                 { id[j] = i; sz[i] += sz[j]; }
-	}
+    // make smaller root point to larger one
+    if   (sz[i] < sz[j]) { id[i] = j; sz[j] += sz[i]; }
+    else                 { id[j] = i; sz[i] += sz[j]; }
+  }
 }
