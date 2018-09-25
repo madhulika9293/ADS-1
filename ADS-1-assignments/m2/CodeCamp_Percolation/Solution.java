2462ec73637cac5f0db943b7355c5cac407e371f
Madhulika <ramyamdhulika9293@msitprogram.net>
2018-09-25 16:13:12 +0530

    "check_style_score -> 24 of 24 passed. style score: 0/1 "

--- ADS-1-assignments/m2/CodeCamp_Percolation/Solution.java
+++ ADS-1-assignments/m2/CodeCamp_Percolation/Solution.java
@@ -38,6 +38,7 @@ class Percolation {
 	 */
 	public Percolation(int num) {
 		grid = new boolean[num][num];
+		// System.out.println(Arrays.deepToString(grid));
 		count = 0;
 		top = 0;
 		bottom = num * num + 1;
@@ -50,10 +51,12 @@ class Percolation {
 	}
 	// open site (row, col) if it is not open already
 	public void open(int row, int col) {
-		if (isOpen(row, col)) {
-			return;
-		} else {
+		// System.out.println(row);
+		// System.out.println(col);
+		// System.out.println(isOpen(row, col));
+		if (!(isOpen(row, col))) {
 			grid[row][col] = true;
+			// System.out.println(Arrays.deepToString(grid));
 			count += 1;
 		}
 		if (row == 0) {
@@ -62,22 +65,23 @@ class Percolation {
 		if (row == n - 1) {
 			percCheck.union(getNum(row, col), bottom);
 		}
-		if (isOpen(row, col - 1) && col > 0) {
+		if (col > 0 && isOpen(row, col - 1)) {
 			percCheck.union(getNum(row, col), getNum(row, col - 1));
 		}
-		if (isOpen(row, col + 1) && col < n - 1) {
+		if (col < n - 1 && isOpen(row, col + 1)) {
 			percCheck.union(getNum(row, col), getNum(row, col + 1));
 		}
-		if (isOpen(row - 1, col) && row > 0) {
+		if (row > 0 && isOpen(row - 1, col)) {
 			percCheck.union(getNum(row, col), getNum(row - 1, col));
 		}
-		if (isOpen(row + 1, col) && row < n - 1) {
+		if (row < n - 1 && isOpen(row + 1, col)) {
 			percCheck.union(getNum(row, col), getNum(row + 1, col));
 		}
+		return;
 	}
 
 	public boolean isOpen(int row, int col) {
-		// is site (row, col) open?
+		// System.out.println(Arrays.deepToString(grid));
 		return grid[row][col];
 	}
 	// public boolean isFull(int row, int col)  // is site (row, col) full?
