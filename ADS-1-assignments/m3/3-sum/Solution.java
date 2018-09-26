c5264fc441299b4538e50018add9840a5b464a66
Madhulika <ramyamdhulika9293@msitprogram.net>
2018-09-26 19:56:12 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- ADS-1-assignments/m3/3-sum/Solution.java
+++ ADS-1-assignments/m3/3-sum/Solution.java
@@ -1,46 +1,57 @@
 import java.util.Scanner;
 import java.util.Arrays;
 
-class threeSum {
-	// private static int[] arr;
+/**
+ * Class for three sum.
+ */
+class ThreeSum {
+  /**
+   * Constructs the object.
+   */
+  private ThreeSum() {
+    // unused constructor
+  }
 
-	// threeSum(int n) {
-	// 	arr = new int[n];
-	// }
-
-	public static int countPairs(final int[] arr) {
-		int count = 0;
-		Arrays.sort(arr);
-		// System.out.println(Arrays.toString(arr));
-		for (int k = 0; k < arr.length; k++) {
-			int i = 0;
-			int j = arr.length - 1;
-			int element = arr[k];
-			while (i != k && j != k && i < j) {
-				int sum = arr[i] + arr[j];
-				if (sum + element > 0) {
-					j -= 1;
-				} else if (sum + element < 0) {
-					i += 1;
-				} else {
-					count += 1;
-					i++;
-				}
-			}
-		}
-		return count;
-	}
+  /**
+   * Counts the number of triplets whose sum is zero in the given array.
+   *
+   * @param      arr   The array.
+   *
+   * @return     Number of triplets.
+   */
+  public static int countTriplets(final int[] arr) {
+    int count = 0;
+    Arrays.sort(arr);
+    // System.out.println(Arrays.toString(arr));
+    for (int k = 0; k < arr.length; k++) {
+      int i = 0;
+      int j = arr.length - 1;
+      int element = arr[k];
+      while (i != k && j != k && i < j) {
+        int sum = arr[i] + arr[j];
+        if (sum + element > 0) {
+          j -= 1;
+        } else if (sum + element < 0) {
+          i += 1;
+        } else {
+          count += 1;
+          i++;
+        }
+      }
+    }
+    return count;
+  }
 }
 
 public final class Solution {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int N = scan.nextInt();
-		int[] inpArr = new int[N];
-		for (int i = 0; i < N; i++) {
-			inpArr[i] = scan.nextInt();
-		}
-		// System.out.println(Arrays.toString(inpArr));
-		System.out.println(threeSum.countPairs(inpArr));
-	}
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    int N = scan.nextInt();
+    int[] inpArr = new int[N];
+    for (int i = 0; i < N; i++) {
+      inpArr[i] = scan.nextInt();
+    }
+    // System.out.println(Arrays.toString(inpArr));
+    System.out.println(ThreeSum.countTriplets(inpArr));
+  }
 }
