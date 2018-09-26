307f2285c2fdec55eaa9dc0254c13556893b460e
Madhulika <ramyamdhulika9293@msitprogram.net>
2018-09-26 21:51:12 +0530

    "check_style_score -> 0 of 3 passed. style score: 0/1 "

--- ADS-1-assignments/m3/Sorted_Array/Solution.java
+++ ADS-1-assignments/m3/Sorted_Array/Solution.java
@@ -2,6 +2,49 @@ import java.util.Scanner;
 import java.util.Arrays;
 
 /**
+ * Class for sorted.
+ */
+class Sorted {
+  /**
+   * Constructs the object.
+   */
+  private Sorted() {
+    // unused
+  }
+
+  /**
+   * Adds the given arrays into a single array.
+   *
+   * @param      n1    The n 1
+   * @param      n2    The n 2
+   *
+   * @return     String representation of the added arrays.
+   */
+  public static String addArr(final int[] n1, final int[] n2) {
+    int len = n1.length + n2.length;
+
+    int[] res = new int[len];
+    int size = 0;
+    int i = 0;
+    int j = 0;
+
+    while (size < len) {
+      if (n1[i] <= n2[j]) {
+        res[size] = n1[i];
+        size += 1;
+        i += 1;
+      }
+      if (n1[i] > n2[j]) {
+        res[size] = n2[j];
+        size += 1;
+        j += 1;
+      }
+    }
+    return Arrays.toString(res);
+  }
+}
+
+/**
  * Class for Solution.
  */
 public final class Solution {
@@ -39,5 +82,7 @@ public final class Solution {
 
     System.out.println(Arrays.toString(arr1));
     System.out.println(Arrays.toString(arr2));
+    System.out.println(Sorted.addArr(arr1, arr2));
   }
 }
+
