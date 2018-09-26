860a9102821c2c7ef435f68a0425e8ff8e546f5d
Madhulika <ramyamdhulika9293@msitprogram.net>
2018-09-26 20:17:11 +0530

    "check_style_score -> 0 of 3 passed. style score: 0/1 "

---
+++ ADS-1-assignments/m3/Sorted_Array/Solution.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+/**
+ * Class for Solution.
+ */
+public final class Solution {
+  /**
+   * Constructs the object.
+   */
+  private Solution() {
+    // unused
+  }
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+
+    int num1 = scan.nextInt();
+    int num2 = scan.nextInt();
+
+    String[] a1 = scan.nextLine().split(",");
+    String[] a2 = scan.nextLine().split(",");
+
+    int[] arr1 = new int[num1];
+    int[] arr2 = new int[num2];
+
+    for (int i = 0; i < num1; i++) {
+      arr1[i] = Integer.parseInt(a1[i]);
+    }
+
+    for (int i = 0; i < num2; i++) {
+      arr2[i] = Integer.parseInt(a2[i]);
+    }
+
+    System.out.println(Arrays.toString(arr1));
+    System.out.println(Arrays.toString(arr2));
+  }
+}
