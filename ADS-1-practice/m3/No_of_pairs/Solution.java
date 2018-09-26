0a4b13c85bab4a8a2c758c98466404555db77ab7
Madhulika <ramyamdhulika9293@msitprogram.net>
2018-09-26 17:29:11 +0530

    m3 - Activity 6

--- ADS-1-practice/m3/No_of_pairs/Solution.java
+++ ADS-1-practice/m3/No_of_pairs/Solution.java
@@ -11,38 +11,46 @@ import java.util.Arrays;
  * Class for Solution.
  */
 public final class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() {
-		// unused constructor
-	}
+  /**
+   * Constructs the object.
+   */
+  private Solution() {
+    // unused constructor
+  }
 
-	/**
-	 * Main function
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int numInp = scan.nextInt();
-		int size = 0;
-		int[] inpArr = new int[numInp];
-		for (int i = 0; i < numInp; i++) {
-			inpArr[size] = scan.nextInt();
-			size += 1;
-		}
+  /**
+   * Main function.
+   * Returns the combinational number of equal pairs in the given array.
+   * @param      args  The arguments
+   */
+  public static void main(final String[] args) {
+    Scanner scan = new Scanner(System.in);
+    int numInp = scan.nextInt();
+    int size = 0;
+    int[] inpArr = new int[numInp];
+    for (int i = 0; i < numInp; i++) {
+      inpArr[size] = scan.nextInt();
+      size += 1;
+    }
 
-		int count = 0;
-		Arrays.sort(inpArr);
-		for (int i = 0; i < inpArr.length - 1; i++) {
-			if (inpArr[i] == inpArr[i + 1]) {
-				count += 1;
-			}
-		}
+    int count = 0;
+    int sum = 0;
+    Arrays.sort(inpArr);
+    for (int i = 0; i < inpArr.length - 1; i++) {
+      if (inpArr[i] == inpArr[i + 1]) {
+        count += 1;
+      } else {
+        sum += (count * (count + 1)) / 2;
+        count = 0;
+      }
 
-		System.out.println("The number of equal pairs in the input are: "
-		                   + (count * (count + 1)) / 2);
-	}
+      if (i == inpArr.length - 2) {
+        sum += (count * (count + 1)) / 2;
+      }
+    }
+
+    System.out.println("The number of equal pairs in the input are: "
+                       + sum);
+  }
 }
 
