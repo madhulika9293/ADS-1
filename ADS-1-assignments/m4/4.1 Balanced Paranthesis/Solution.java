92f6a630debcb70bae75b3e296f7bcd647592fc6
Madhulika <ramyamdhulika9293@msitprogram.net>
2018-09-27 18:51:14 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

--- ADS-1-assignments/m4/4.1 Balanced Paranthesis/Solution.java
+++ ADS-1-assignments/m4/4.1 Balanced Paranthesis/Solution.java
@@ -2,140 +2,32 @@ import java.util.Scanner;
 import java.util.Arrays;
 
 /**
- * Class for implementing linked list.
- *
- * @param      <E>   generic data type.
- */
-class LinkedList<E> {
-  /**
-   * Class for node.
-   */
-  private class Node {
-    /**
-     * data of generic data type.
-     */
-    E data;
-    /**
-     * Node object for pointing to the next node.
-     */
-    Node next;
-
-    /**
-     * Default empty constructor.
-     */
-    Node() {}
-
-    /**
-     * Constructs the object - overloaded.
-     *
-     * @param      data  The data
-     */
-    Node(E data1) {
-      this.data = data1;
-      this.next = null;
-    }
-
-    /**
-     * Constructs the object - overloaded.
-     *
-     * @param      data1  The data
-     * @param      next1  The next node pointer.
-     */
-    Node(E data1, Node next1) {
-      this.data = data1;
-      this.next = next1;
-    }
-  }
-  /**
-   * Objects of type node for keeping track of head & tail of the list.
-   */
-  private Node head, tail;
-
-  /**
-   * Gets the head.
-   *
-   * @return     The head.
-   */
-  public void getHead() {
-    System.out.println(head.data);
-  }
-
-  /**
-   * Gets the tail.
-   *
-   * @return     The tail.
-   */
-  public void getTail() {
-    System.out.println(tail.data);
-  }
-  /**
-   * Adds data at head.
-   *
-   * @param      dt    data of type generic to be added to the list.
-   */
-  public void addAtHead(final E dt) {
-    Node temp = new Node(dt, head);
-    if (head == null) {
-      head = temp;
-      tail = head;
-      return;
-    }
-    head = temp;
-  }
-
-  /**
-   * Adds an at tail.
-   *
-   * @param      dt    data to be stored in the list.
-   */
-  public void addAtTail(final E dt) {
-    tail.next = new Node(dt);
-    tail = tail.next;
-  }
-
-  /**
-   * Pops a value from the head of the list.
-   *
-   * @return     Last value which is of generic type.
-   */
-  public E popAtHead() {
-    E out = head.data;
-    head = head.next;
-    return out;
-  }
-
-  /**
-   * Pops a node at the tail of the list.
-   *
-   * @return     Data of the popped node.
-   */
-  public E popAtTail() {
-    Node temp = head;
-    while (temp != null && temp.next != tail) {
-      temp = temp.next;
-    }
-    E element = temp.next.data;
-    temp.next = null;
-    tail = temp;
-    return element;
-  }
-
-  public void print() {
-    Node temp = new Node();
-    temp = head;
-    while (temp.next != null) {
-      System.out.print(temp.data + " ");
-      temp = temp.next;
-    }
-    System.out.print(temp.data);
-    System.out.println();
-  }
-}
-
-/**
  * Class for solution. This implements the linked list for our problem.
  */
 public final class Solution {
+
+  public static boolean balParen(final String s) {
+
+    Stack<String> sInp = new Stack<String>();
+
+    String[] sArr = s.split("");
+
+    for (int i = 0; i < s.length(); i++) {
+
+      if (sArr[i].equals("[") || sArr[i].equals("{") || sArr[i].equals("(")) {
+        sInp.push(sArr[i]);
+      } else if (sArr[i].equals("]") && sInp.pop() != "[") {
+        return false;
+      } else if (sArr[i].equals("}") && sInp.pop() != "{") {
+        return false;
+      } else if (sArr[i].equals(")") && sInp.pop() != "(") {
+        return false;
+      }
+    }
+
+    return true;
+
+  }
 
   public static void main(String[] args) {
 
@@ -145,26 +37,13 @@ public final class Solution {
 
     for (int i = 0; i < num; i++) {
 
-      LinkedList<String> lstB = new LinkedList<>();
+      String inp = scan.nextLine();
 
-      String[] inp = scan.nextLine().split("");
-      // System.out.println(Arrays.toString(inp));
-
-      String out = "YES";
-
-      for (int j = 0; j < inp.length; j++) {
-        if (inp[j] == "[" || inp[j] == "{" || inp[j] == "(") {
-          lstB.addAtHead(inp[j]);
-        } else if (inp[j] == "]" && lstB.popAtHead() != "[") {
-          out = "NO";
-        } else if (inp[j] == "}" && lstB.popAtHead() != "{") {
-          out = "NO";
-        } else if (inp[j] == ")" && lstB.popAtHead() != "(") {
-          out = "NO";
-        }
+      if (balParen(inp)) {
+        System.out.println("YES");
+      } else {
+        System.out.println("NO");
       }
-
-      System.out.println(out);
 
     }
   }
