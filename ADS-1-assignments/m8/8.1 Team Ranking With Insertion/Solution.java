bca1e21a1ae59873a65b29fe135c1d28ce617ce9
Madhulika <ramyamdhulika9293@msitprogram.net>
2018-10-02 18:32:39 +0530

    "check_style_score -> 6 of 6 passed. style score: 0/1 "

--- ADS-1-assignments/m8/8.1 Team Ranking With Insertion/Solution.java
+++ ADS-1-assignments/m8/8.1 Team Ranking With Insertion/Solution.java
@@ -1,12 +1,25 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
+/**
+ * Class for Solution.
+ */
 public final class Solution {
 
+	/**
+	 * Constructs the object.
+	 */
 	private Solution() {
 		// unused constructor
 	}
 
+	/**
+	 * Prints out the leader board of given cricket teams.
+	 *
+	 * @param      tms   The tms
+	 * @param      sz    The size
+	 *
+	 * @return     String representation of the leader board.
+	 */
 	public static String leaderBoard(final Team[] tms, final int sz) {
 		InsertionSortTeam.sort(tms, sz);
 
@@ -19,11 +32,19 @@ public final class Solution {
 		return out;
 	}
 
-	public static void main(String[] args) {
+	/**
+	 * Main method.
+	 *
+	 * @param      args  The arguments
+	 */
+	public static void main(final String[] args) {
 
 		Scanner scan = new Scanner(System.in);
 
-		Team[] teams = new Team[10];
+		final int three = 3;
+		final int ten = 10;
+
+		Team[] teams = new Team[ten];
 		int size = 0;
 
 		while (scan.hasNext()) {
@@ -31,7 +52,7 @@ public final class Solution {
 			Team temp = new Team(inp[0],
 			                     Integer.parseInt(inp[1]),
 			                     Integer.parseInt(inp[2]),
-			                     Integer.parseInt(inp[3]));
+			                     Integer.parseInt(inp[three]));
 			teams[size] = temp;
 			size += 1;
 		}
--- ADS-1-assignments/m8/8.1 Team Ranking With Insertion/Team.java
+++ ADS-1-assignments/m8/8.1 Team Ranking With Insertion/Team.java
@@ -1,11 +1,35 @@
 import java.util.Arrays;
 
+/**
+ * Class for team object.
+ * We use comparable interface to implement sort algortihms.
+ */
 public class Team implements Comparable<Team> {
+	/**
+	 * team name.
+	 */
 	String teamName;
+	/**
+	 * wins for the team.
+	 */
 	int wins;
+	/**
+	 * losses for the team.
+	 */
 	int losses;
+	/**
+	 * draws for the team.
+	 */
 	int draws;
 
+	/**
+	 * Constructs the object.
+	 *
+	 * @param      tN    The t n
+	 * @param      w     wins
+	 * @param      l     losses
+	 * @param      d     draws.
+	 */
 	Team(final String tN, final int w, final int l, final int d) {
 		teamName = tN;
 		wins = w;
@@ -13,6 +37,14 @@ public class Team implements Comparable<Team> {
 		draws = d;
 	}
 
+	/**
+	 * Compares two objects of this class.
+	 * Complexity Analysis: O(1).
+	 *
+	 * @param      other  The other
+	 *
+	 * @return     int: 1 if greater, 0 if equal & -1 if lesser.
+	 */
 	public int compareTo (Team other) {
 		if (this.wins > other.wins) return 1;
 		if (this.wins < other.wins) return -1;
@@ -23,23 +55,59 @@ public class Team implements Comparable<Team> {
 		else return 0;
 	}
 
+	/**
+	 * Returns a string representation of the object.
+	 *
+	 * @return     String representation of the object.
+	 */
 	public String toString() {
 		return teamName;
 	}
 
 }
 
+/**
+ * Class for insertion sort of teams.
+ */
 class InsertionSortTeam {
+	/**
+	 * To check if the object is greater.
+	 *
+	 * @param      a     team 1
+	 * @param      b     team 2
+	 *
+	 * @return     returns true if team 1 is greater than
+	 * team 2 on the leader board.
+	 */
 	static boolean more (Comparable a, Comparable b) {
 		return a.compareTo(b) > 0;
 	}
 
+	/**
+	 * Swaps 2 objects in the array of objects.
+	 *
+	 * @param      a     Array of objects.
+	 * @param      i     Object 1.
+	 * @param      j     Object 2.
+	 */
 	static void swap ( Comparable[] a, int i, int j) {
 		Comparable temp = a[i];
 		a[i] = a[j];
 		a[j] = temp;
 	}
 
+	/**
+	 * Sorts the array of objects using Insertion Sort.
+	 *
+	 * Best Case: N-1 compares and 0 exchanges.
+	 *
+	 * Average Case: N^2/4 compares and N^2/4 exchanges.
+	 *
+	 * Worst Case: N^2/2 compares and N^2/2 exchanges.
+	 *
+	 * @param      arr   The array of objects.
+	 * @param      size  The number of objects in the array.
+	 */
 	static void sort(Comparable[] arr, int size) {
 		for (int i = 0; i < size; i++) {
 			for (int j = i; j > 0; j--) {
