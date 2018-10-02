759b37c47e32d4901ee2cd7b6309365a1537e3f0
Madhulika <ramyamdhulika9293@msitprogram.net>
2018-10-02 19:06:47 +0530

    "check_style_score -> 6 of 6 passed. style score: 1/1 "

---
+++ ADS-1-assignments/m8/8.2 Team Ranking With Selection/Solution.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+
+/**
+ * Class for Solution.
+ */
+public final class Solution {
+
+  /**
+   * Constructs the object.
+   */
+  private Solution() {
+    // unused constructor
+  }
+
+  /**
+   * Prints out the leader board of given cricket teams.
+   *
+   * @param      tms   The tms
+   * @param      sz    The size
+   *
+   * @return     String representation of the leader board.
+   */
+  public static String leaderBoard(final Team[] tms, final int sz) {
+    SelectionSortTeam.sort(tms, sz);
+
+    String out = "";
+    for (int i = 0; i < sz; i++) {
+      out += tms[i].toString();
+      out += ",";
+    }
+    out = out.substring(0, out.length() - 1);
+    return out;
+  }
+
+  /**
+   * Main method.
+   *
+   * @param      args  The arguments
+   */
+  public static void main(final String[] args) {
+
+    Scanner scan = new Scanner(System.in);
+
+    final int three = 3;
+    final int ten = 10;
+
+    Team[] teams = new Team[ten];
+    int size = 0;
+
+    while (scan.hasNext()) {
+      String[] inp = scan.nextLine().split(",");
+      Team temp = new Team(inp[0],
+                           Integer.parseInt(inp[1]),
+                           Integer.parseInt(inp[2]),
+                           Integer.parseInt(inp[three]));
+      teams[size] = temp;
+      size += 1;
+    }
+    scan.close();
+    System.out.println(leaderBoard(teams, size));
+  }
+}
---
+++ ADS-1-assignments/m8/8.2 Team Ranking With Selection/Team.java
@@ -0,0 +1,123 @@
+import java.util.Arrays;
+
+/**
+ * Class for team object.
+ * We use comparable interface to implement sort algortihms.
+ */
+public class Team implements Comparable<Team> {
+	/**
+	 * team name.
+	 */
+	String teamName;
+	/**
+	 * wins for the team.
+	 */
+	int wins;
+	/**
+	 * losses for the team.
+	 */
+	int losses;
+	/**
+	 * draws for the team.
+	 */
+	int draws;
+
+	/**
+	 * Constructs the object.
+	 *
+	 * @param      tN    The t n
+	 * @param      w     wins
+	 * @param      l     losses
+	 * @param      d     draws.
+	 */
+	Team(final String tN, final int w, final int l, final int d) {
+		teamName = tN;
+		wins = w;
+		losses = l;
+		draws = d;
+	}
+
+	/**
+	 * Compares two objects of this class.
+	 * Complexity Analysis: O(1).
+	 *
+	 * @param      other  The other
+	 *
+	 * @return     int: 1 if greater, 0 if equal & -1 if lesser.
+	 */
+	public int compareTo (Team other) {
+		if (this.wins > other.wins) return 1;
+		if (this.wins < other.wins) return -1;
+		if (this.losses < other.losses) return 1;
+		if (this.losses > other.losses) return -1;
+		if (this.draws > other.draws) return 1;
+		if (this.draws < other.draws) return -1;
+		else return 0;
+	}
+
+	/**
+	 * Returns a string representation of the object.
+	 *
+	 * @return     String representation of the object.
+	 */
+	public String toString() {
+		return teamName;
+	}
+
+}
+
+/**
+ * Class for insertion sort of teams.
+ */
+class SelectionSortTeam {
+	/**
+	 * To check if the object is greater.
+	 *
+	 * @param      a     team 1
+	 * @param      b     team 2
+	 *
+	 * @return     returns true if team 1 is greater than
+	 * team 2 on the leader board.
+	 */
+	static boolean more (Comparable a, Comparable b) {
+		return a.compareTo(b) > 0;
+	}
+
+	/**
+	 * Swaps 2 objects in the array of objects.
+	 *
+	 * @param      a     Array of objects.
+	 * @param      i     Object 1.
+	 * @param      j     Object 2.
+	 */
+	static void swap ( Comparable[] a, int i, int j) {
+		Comparable temp = a[i];
+		a[i] = a[j];
+		a[j] = temp;
+	}
+
+	/**
+	 * Sorts the array of objects using Insertion Sort.
+	 *
+	 * Best Case: N^2.
+	 *
+	 * Average Case: N^2.
+	 *
+	 * Worst Case: N^2.
+	 *
+	 * @param      arr   The array of objects.
+	 * @param      size  The number of objects in the array.
+	 */
+	static void sort(Comparable[] arr, int size) {
+		for (int i = 0; i < size; i++) {
+			int min = i;
+			for (int j = i + 1; j < size; j++) {
+				if (more(arr[j], arr[min])) {
+					min = j;
+				}
+			}
+			swap(arr, i, min);
+			// System.out.println(Arrays.toString(arr));
+		}
+	}
+}
