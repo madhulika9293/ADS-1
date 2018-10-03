d78f304fbeb69ac18ed848337dd45beb56870dd0
Madhulika <ramyamdhulika9293@msitprogram.net>
2018-10-03 22:34:03 +0530

    "check_style_score -> 4 of 4 passed. style score: 1/1 "

--- ADS-1-assignments/m9/9.1 Merge Sort Improvements/Solution.java
+++ ADS-1-assignments/m9/9.1 Merge Sort Improvements/Solution.java
@@ -1,29 +1,34 @@
 import java.util.Scanner;
-import java.util.Arrays;
-
 /**
  * Class for solution.
  */
-public final class Solution {
-  /**
-   * Constructs the object.
-   */
-  private Solution() {
-    // unused constructor
-  }
-  /**
-   * Main method.
-   *
-   * @param      args  The arguments
-   */
-  public static void main(final String[] args) {
-    Scanner scan = new Scanner(System.in);
-    while (scan.hasNext()) {
-      String[] inputs = scan.nextLine().split(",");
-      // Integer[] inpInt = new Integer[inputs.length];
-      MergeSort.sort(inputs);
-      System.out.println(Arrays.toString(inputs) + "\n");
+final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        //Constructor.
     }
-    scan.close();
-  }
+    /**
+     * main.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Mergeimp m = new Mergeimp();
+        while (sc.hasNextLine()) {
+            String[] tokens = sc.nextLine().split(",");
+            m.sort(tokens);
+            String str = "[";
+            int i;
+            for (i = 0; i < tokens.length - 1; i++) {
+                str += tokens[i] + ", ";
+            }
+            str += tokens[i] + "]";
+            System.out.println(str);
+            System.out.println();
+        }
+    }
 }
+
