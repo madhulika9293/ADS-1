6dfb26d11a7a71b37239fd867e8f535cca71be03
Madhulika <ramyamdhulika9293@msitprogram.net>
2018-10-11 18:02:24 +0530

    "ads1_m14_a2: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS-1-assignments/m14/14.2 student reservation system/HeapSort.java
+++ ADS-1-assignments/m14/14.2 student reservation system/HeapSort.java
@@ -1,7 +1,7 @@
 /**
  * Class for heap.
  */
-public class HeapSort {
+public final class HeapSort {
 
     /**
      * Constructs the object.
@@ -15,39 +15,45 @@ public class HeapSort {
      */
     public static void sort(final Student[] pq) {
         int n = pq.length;
-        for (int k = n / 2; k >= 1; k--)
+        for (int k = n / 2; k >= 1; k--) {
             sink(pq, k, n);
+        }
         while (n > 1) {
             exch(pq, 1, n--);
             sink(pq, 1, n);
         }
     }
 
-    /***************************************************************************
+    /**********************************************************
      * Helper functions to restore the heap invariant.
-     ***************************************************************************/
+     **********************************************************/
 
     /**
      * To sink the array with an element.
      *
      * @param      pq    Input array.
-     * @param      k     Index.
+     * @param      k1     Index.
      * @param      n     Size of the array.
      */
-    private static void sink(Student[] pq, int k, int n) {
+    private static void sink(final Student[] pq, final int k1, final int n) {
+        int k = k1;
         while (2 * k <= n) {
             int j = 2 * k;
-            if (j < n && more(pq, j, j + 1)) j++;
-            if (!more(pq, k, j)) break;
+            if (j < n && more(pq, j, j + 1)) {
+                j++;
+            }
+            if (!more(pq, k, j)) {
+                break;
+            }
             exch(pq, k, j);
             k = j;
         }
     }
 
-    /***************************************************************************
+    /**************************************************************
      * Helper functions for comparisons and swaps.
      * Indices are "off-by-one" to support 1-based indexing.
-     ***************************************************************************/
+     **************************************************************/
     /**
      * Compares two objects.
      *
@@ -57,7 +63,7 @@ public class HeapSort {
      *
      * @return     true if more, false otherwise.
      */
-    private static boolean more(Student[] pq, int i, int j) {
+    private static boolean more(final Student[] pq, final int i, final int j) {
         return pq[i - 1].compareTo(pq[j - 1]) > 0;
     }
 
@@ -68,7 +74,7 @@ public class HeapSort {
      * @param      i     Object 1.
      * @param      j     Object 2.
      */
-    private static void exch(Student[] pq, int i, int j) {
+    private static void exch(final Student[] pq, final int i, final int j) {
         Student swap = pq[i - 1];
         pq[i - 1] = pq[j - 1];
         pq[j - 1] = swap;
