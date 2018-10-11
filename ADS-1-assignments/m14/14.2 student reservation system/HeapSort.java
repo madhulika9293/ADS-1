388c04e8fe82014842aa60007205b863af2e3d56
Madhulika <ramyamdhulika9293@msitprogram.net>
2018-10-11 17:35:25 +0530

    "ads1_m14_a2: 0/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS-1-assignments/m14/14.2 student reservation system/HeapSort.java
+++ ADS-1-assignments/m14/14.2 student reservation system/HeapSort.java
@@ -12,7 +12,7 @@ public class HeapSort {
      * Rearranges the array in ascending order, using the natural order.
      * @param pq the array to be sorted
      */
-    public static void sort(Comparable[] pq) {
+    public static void sort(Student[] pq) {
         int n = pq.length;
         for (int k = n/2; k >= 1; k--)
             sink(pq, k, n);
@@ -26,7 +26,7 @@ public class HeapSort {
     * Helper functions to restore the heap invariant.
     ***************************************************************************/
 
-    private static void sink(Comparable[] pq, int k, int n) {
+    private static void sink(Student[] pq, int k, int n) {
         while (2*k <= n) {
             int j = 2*k;
             if (j < n && less(pq, j, j+1)) j++;
@@ -40,12 +40,12 @@ public class HeapSort {
     * Helper functions for comparisons and swaps.
     * Indices are "off-by-one" to support 1-based indexing.
     ***************************************************************************/
-    private static boolean less(Comparable[] pq, int i, int j) {
+    private static boolean less(Student[] pq, int i, int j) {
         return pq[i-1].compareTo(pq[j-1]) < 0;
     }
 
-    private static void exch(Object[] pq, int i, int j) {
-        Object swap = pq[i-1];
+    private static void exch(Student[] pq, int i, int j) {
+        Student swap = pq[i-1];
         pq[i-1] = pq[j-1];
         pq[j-1] = swap;
     }
